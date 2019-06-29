37c96adaada140b9eba43b3df97412f84f6f0836
Andy Bettisworth <andy@accreu.com>
2019-06-28 23:43:55 -0500

    Simple Link component

---
+++ Link/Link.js
@@ -0,0 +1,21 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+
+/**
+ * Define component
+ */
+
+function Link() {
+  return (
+    <a href="#">Link</a>
+  )
+};
+
+/**
+ * Export component
+ */
+
+export default Link;
