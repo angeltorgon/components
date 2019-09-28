03ebb8af19ad06cfd7bdc750f2d9f76a41d62e45
Andy Bettisworth <andy@accreu.com>
2019-09-28 05:34:25 -0500

    Add Column helper methods

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,10 +5,33 @@
 import styled from 'styled-components';
 
 /**
+ * Define helpers
+ */
+
+function calc_width(n) {
+  if (n) {
+    n = Math.round((size / 12) * 10e7) / 10e5
+    if (n < 0) n = 0
+    if (n > 100) n = 100
+    return `${n}%`
+  } else {
+    return '100%'
+  }
+}
+
+function calc_spacing(n) {
+  if (n <= 0) return
+  return `padding: ${n * 4}px;`
+}
+
+/**
  * Define component
  */
 
 const Column = styled.div`
+  flex: 0 0 ${props => calc_width(props.width)};
+  max-width: ${props => calc_width(props.width)};
+  ${props => calc_spacing(props.spacing)}
 `
 
 /**
