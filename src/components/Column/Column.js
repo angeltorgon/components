b999a87fd3f4b28817a65fc3b37019b8d19392d7
Andy Bettisworth <andy@accreu.com>
2019-09-28 05:15:59 -0500

    Add Column component

---
+++ src/components/Column/Column.js
@@ -0,0 +1,18 @@
+/**
+ * Dependencies
+ */
+
+import styled from 'styled-components';
+
+/**
+ * Define component
+ */
+
+const Column = styled.div`
+`
+
+/**
+ * Export component
+ */
+
+export default Column;
