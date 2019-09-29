bbf5211df388174649bc9c260664347e320a7829
Andy Bettisworth <andy@accreu.com>
2019-09-29 04:32:38 -0500

    Implement Paragraph via styled-components

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -2,17 +2,21 @@
  * Dependencies
  */
 
-import React from 'react';
+import styled from 'styled-components';
 
 /**
  * Define component
  */
 
-function Paragraph(props) {
-  return (
-    <p>{props.children}</p>
-  )
-};
+const Paragraph = styled.p`
+  ${props => props.fontFamily ? `font-family: ${props.fontFamily};` : 'font-family: inherit;'}
+  ${props => props.lineHeight ? `line-height: ${props.lineHeight};` : 'line-height: inherit;'}
+  ${props => props.letterSpacing ? `letter-spacing: ${props.letterSpacing};` : 'letter-spacing: inherit;'}
+  ${props => props.align ? `text-align: ${props.align};` : 'text-align: inherit;'}
+  ${props => props.weight ? `font-weight: ${props.weight};` : 'font-weight: inherit;'}
+  ${props => props.size ? `font-size: ${props.size};` : 'font-size: inherit;'}
+  ${props => props.color ? `color: ${props.color};` : 'color: inherit;'}
+`
 
 /**
  * Export component
