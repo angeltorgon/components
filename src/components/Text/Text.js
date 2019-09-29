442e13b81733a149f8c6623d37bdb7f5703cc8e6
Andy Bettisworth <andy@accreu.com>
2019-09-29 03:29:38 -0500

    Dynamically handle font-family for Text

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -9,6 +9,7 @@ import styled from 'styled-components';
  */
 
 const Text = styled.span`
+  ${props => props.fontFamily ? `font-family: ${props.fontFamily};` : 'font-family: inherit;'}
   ${props => props.weight ? `font-weight: ${props.weight};` : 'font-weight: normal;'}
   ${props => props.size ? `font-size: ${props.size};` : 'font-size: medium;'}
   ${props => props.color ? `color: ${props.color};` : 'color: inherit;'}
