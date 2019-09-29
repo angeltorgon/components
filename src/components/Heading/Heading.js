8888d2fa0de5a57df2093c67718538037d6d698d
Andy Bettisworth <andy@accreu.com>
2019-09-29 15:21:32 -0500

    Add gradient for Heading font-size

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -10,14 +10,34 @@ import styled from 'styled-components';
  */
 
 const Component = styled.h2`
-  font-family: ${props => props.fontFamily ? props.fontFamily : 'inherit'};
-  line-height: ${props => props.lineHeight ? props.lineHeight : 'inherit'};
-  letter-spacing: ${props => props.letterSpacing ? props.letterSpacing : 'inherit'};
-  text-align: ${props => props.align ? props.align : 'inherit'};
-  font-weight: ${props => props.weight ? props.weight : 'inherit'};
-  font-size: ${props => props.size ? props.size : 'inherit'};
-  color: ${props => props.color ? props.color : 'inherit'};
-  background-color: ${props => props.backgroundColor ? props.backgroundColor : 'inherit'};
+  font-weight: ${props => props.weight ? props.weight : '400'};
+  margin: ${props => props.margin ? props.margin : '0'};
+
+  font-family: ${props => props.fontFamily ? props.fontFamily : ''};
+  line-height: ${props => props.lineHeight ? props.lineHeight : ''};
+  letter-spacing: ${props => props.letterSpacing ? props.letterSpacing : ''};
+  text-align: ${props => props.align ? props.align : ''};
+  color: ${props => props.color ? props.color : ''};
+  background-color: ${props => props.backgroundColor ? props.backgroundColor : ''};
+`
+  
+const ComponentH1 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '4.5rem'};
+`
+const ComponentH2 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '3rem'};
+`
+const ComponentH3 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '2.3rem'};
+`
+const ComponentH4 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '1.8rem'};
+`
+const ComponentH5 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '1.5rem'};
+`
+const ComponentH6 = styled(Component)`
+  font-size: ${props => props.size ? props.size : '1.25rem'};
 `
 
 /**
@@ -28,31 +48,31 @@ function Heading(props) {
   switch (props.type) {
     case 'h1':
       return (
-        <Component as="h1" {...props}>{props.children}</Component>
+        <ComponentH1 as="h1" {...props}>{props.children}</ComponentH1>
       )
     case 'h2':
       return (
-        <Component as="h2" {...props}>{props.children}</Component>
+        <ComponentH2 as="h2" {...props}>{props.children}</ComponentH2>
       )
     case 'h3':
       return (
-        <Component as="h3" {...props}>{props.children}</Component>
+        <ComponentH3 as="h3" {...props}>{props.children}</ComponentH3>
       )
     case 'h4':
       return (
-        <Component as="h4" {...props}>{props.children}</Component>
+        <ComponentH4 as="h4" {...props}>{props.children}</ComponentH4>
       )
     case 'h5':
       return (
-        <Component as="h5" {...props}>{props.children}</Component>
+        <ComponentH5 as="h5" {...props}>{props.children}</ComponentH5>
       )
     case 'h6':
       return (
-        <Component as="h6" {...props}>{props.children}</Component>
+        <ComponentH6 as="h6" {...props}>{props.children}</ComponentH6>
       )
     default:
       return (
-        <Component as="h2" {...props}>{props.children}</Component>
+        <ComponentH2 as="h2" {...props}>{props.children}</ComponentH2>
       )
   }
 };
